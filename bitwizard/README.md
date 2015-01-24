b9f4e06008977cee81c852febf6ff7a664528533
vaxxi <github@vaxxi.net>
2015-01-24 20:47:57 +0000

    Add random string example and decrease write duration

--- bitwizard/README.md
+++ bitwizard/README.md
@@ -1,16 +1,16 @@
-# Code for Raspberry Pi add-on boards from bitwizard.nl
+# Add-on boards from bitwizard.nl
 
 You need to have the [wiringPi library](http://wiringpi.com/) installed (no reinventing the wheel).
 
 Clone the repo, run `make`.
 
-## rpi_ui
+## rpi_ui.c
 
 [Board details](http://bitwizard.nl/wiki/index.php/User_Interface)
 
-Sample code in `rpi_ui.c`.
+Edit your SPI channel, your rpi_ui SPI address and enable DEBUG if you wish.
 
-## bigrelay
+## bigrelay.c
 
 [Board details](http://bitwizard.nl/wiki/index.php/Relay)
 
--- bitwizard/rpi_ui.c
+++ bitwizard/rpi_ui.c
@@ -1,8 +1,30 @@
 #include <rpi_ui.h>
 
+
+// random string generator
+// credit: http://stackoverflow.com/questions/15767691/whats-the-c-library-function-to-generate-random-string
+
+void rand_str(char *dest, size_t length) {
+  char charset[] = "0123456789"
+    "abcdefghijklmnopqrstuvwxyz"
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    "!@#$%^&*()_+";
+
+while (length-- > 0) {
+  size_t index = (double) rand() / RAND_MAX * (sizeof charset - 1);
+  *dest++ = charset[index];
+  }
+  *dest = '\0';
+}
+                
+
 void main() {
 
 int i=0,j=0;
+char lcd_text[32];
+
+// generate a random string to print
+rand_str(lcd_text,ARRAY_SIZE(lcd_text));
 
 // set up the LCD screen
 lcd_setup();
@@ -12,21 +34,21 @@ lcd_clr_src();
 
 // change brightness two times, sleep 2 seconds in between
 lcd_brightness_write(25);
-sleep(2);
+sleep(1);
 lcd_brightness_write(50);
-sleep(2);
+sleep(1);
 // change contrast two times, sleep 2 seconds in between
 lcd_contrast_write(25);
-sleep(2);
+sleep(1);
 lcd_contrast_write(50);
-sleep(2);
+sleep(1);
 
 // go through each LCD character and print a character
 for (i=1;i<3;i++)
   for (j=1;j<17;j++) {
-    sleep(1);
+    usleep(250000);
     lcd_cursor_move(i,j);
-    lcd_put_char('*');
+    lcd_put_char(lcd_text[(i-1)*16+j-1]);     
   }
   
 }
